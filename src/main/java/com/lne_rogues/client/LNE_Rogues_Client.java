8ee650d196ec31679abbc283c70713ef91a407c3
ProfessorFichte <70310161+ProfessorFichte@users.noreply.github.com>
2025-04-05 10:50:36 +0200

    Add Dancing Dagger Model

---
+++ src/main/java/com/lne_rogues/client/LNE_Rogues_Client.java
@@ -0,0 +1,22 @@
+package com.lne_rogues.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.util.Identifier;
+import net.spell_engine.api.render.CustomModels;
+
+import java.util.List;
+
+import static com.lne_rogues.LNE_Rogues_Mod.MOD_ID;
+
+@Environment(EnvType.CLIENT)
+public class LNE_Rogues_Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        CustomModels.registerModelIds(List.of(
+                new Identifier(MOD_ID, "projectile/dancing_dagger")
+        ));
+    }
+}
